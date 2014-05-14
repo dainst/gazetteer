bb74a13b06fe4caad65684667e37f2aa0d648be6
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2014-05-14 15:06:34 +0200

    fixed errors that occurred when mongodb river was created for the first time

--- src/main/java/org/dainst/gazetteer/search/ElasticSearchService.java
+++ src/main/java/org/dainst/gazetteer/search/ElasticSearchService.java
@@ -2,6 +2,7 @@ package org.dainst.gazetteer.search;
 
 import java.io.InputStream;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 import org.dainst.gazetteer.dao.PlaceRepository;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
@@ -11,6 +12,8 @@ import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.Requests;
+import org.elasticsearch.indices.IndexAlreadyExistsException;
+import org.elasticsearch.indices.TypeMissingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,14 +40,25 @@ public class ElasticSearchService {
 		
 		LOGGER.info("reindexing all places");
 		
-		try {
-			
+		try {			
 			// recreate es index
 			client.admin().indices().delete(new DeleteIndexRequest("gazetteer")).get();
-			client.admin().indices().create(new CreateIndexRequest("gazetteer")).get();
+			client.admin().indices().create(new CreateIndexRequest("gazetteer")).get();			
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to reindex places", e);
+		}
 			
-			// recreate corresponding mongodb river
+		// recreate corresponding mongodb river
+		try {
+			client.admin().indices().create(new CreateIndexRequest("_river")).get();
 			client.admin().indices().deleteMapping(new DeleteMappingRequest("_river").types("mongodb")).get();
+		} catch (InterruptedException e) {
+			throw new RuntimeException("Failed to reindex places", e);
+		} catch (ExecutionException e) {
+			// eat exceptions if river is created for the first time
+		}
+		
+		try {
 			InputStream esRiverStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("es/mongodb_river.json");
 			Scanner s = new Scanner(esRiverStream);
 			String esRiverJson = s.useDelimiter("\\A").next();
@@ -53,7 +67,6 @@ public class ElasticSearchService {
 			LOGGER.debug("esRiverJson: {}", esRiverJson);
 			IndexResponse indexResponse = client.index(Requests.indexRequest("_river").type("mongodb").id("_meta").source(esRiverJson)).get();
 			LOGGER.debug("created: {}", indexResponse.isCreated());
-			
 		} catch (Exception e) {
 			throw new RuntimeException("Failed to reindex places", e);
 		}
