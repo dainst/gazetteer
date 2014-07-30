bca97bed3c9d99280df623b23e7379c83d609b4e
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2014-07-30 16:47:36 +0200

    sort list of languages

--- src/main/java/org/dainst/gazetteer/helpers/LocalizedLanguagesHelper.java
+++ src/main/java/org/dainst/gazetteer/helpers/LocalizedLanguagesHelper.java
@@ -1,6 +1,11 @@
 package org.dainst.gazetteer.helpers;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -28,7 +33,39 @@ public class LocalizedLanguagesHelper {
 				localizedLanguages.put(language, messageSource.getMessage("languages."+language, null, Locale.GERMAN));
 			}
 		}
-		return localizedLanguages;
+		
+		return sortHashMapByValues(localizedLanguages);
 	}
+	
+	private LinkedHashMap<String,String> sortHashMapByValues(HashMap<String,String> passedMap) {
+		   List<String> mapKeys = new ArrayList<String>(passedMap.keySet());
+		   List<String> mapValues = new ArrayList<String>(passedMap.values());
+		   Collections.sort(mapValues);
+		   Collections.sort(mapKeys);
+
+		   LinkedHashMap<String,String> sortedMap = new LinkedHashMap<String,String>();
+
+		   Iterator<String> valueIt = mapValues.iterator();
+		   while (valueIt.hasNext()) {
+		       String val = valueIt.next();
+		       Iterator<String> keyIt = mapKeys.iterator();
+
+		       while (keyIt.hasNext()) {
+		           String key = keyIt.next();
+		           String comp1 = passedMap.get(key);
+		           String comp2 = val;
+
+		           if (comp1.equals(comp2)){
+		               passedMap.remove(key);
+		               mapKeys.remove(key);
+		               sortedMap.put(key, val);
+		               break;
+		           }
+
+		       }
+
+		   }
+		   return sortedMap;
+		}
 
 }
