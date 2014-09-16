e6ece499595f02b6e51136bc4a1f0331c5a434d3
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2014-09-16 18:02:03 +0200

    added general import script

---
+++ src/main/scripts/import.rb
@@ -0,0 +1,224 @@
+#!/usr/bin/env ruby
+require 'optparse'
+require 'rest-client'
+require 'json'
+require 'csv'
+require 'pp'
+require 'highline/import'
+
+
+# Standard parameter values
+
+gaz_uri = "http://gazetteer.dainst.org"
+
+template = '{
+  "prefName": {
+    "title": "#{_[0]}",
+    "language": "#{_[1]}",
+    "ancient": #{_[2].gsub(/yes/,\'true\').gsub(/no/,\'false\')}
+  },
+  "types": [ "#{_[3]}" ],
+  "prefLocation": {
+    "coordinates": [ #{_[4].gsub(/,/,\'.\').to_f}, #{_[5].gsub(/,/,\'.\').to_f} ]
+  },
+  "ids": [
+    {
+      "value": "#{_[6]}",
+      "context": "geonames"
+    },
+    {
+      "value": "#{_[7]}",
+      "context": "pleiades"
+    }
+  ],
+  "parent": "http://gazetteer.dainst.org/place/#{_[8]}"
+}'
+
+
+# option parsing
+
+options = OpenStruct.new
+OptionParser.new do |opts|
+  
+  opts.banner = "Usage: example.rb [options] [file ...]"
+
+  opts.on("-u", "--username USERNAME", "Gazetteer user name") do |u|
+    options.user = u
+  end
+
+  opts.on("-p", "--password [PASSWORD]", "Gazetteer user password") do |p|
+    if p
+      options.password = p
+    else
+      ask("Gazetteer password:") { |q| q.echo = false }
+    end
+  end
+
+  options.uri = gaz_uri
+  opts.on("-U", "--uri URI", "Use URI as gazetteer base uri (standard: \"#{gaz_uri}\")") do |u|
+  	options.uri = u
+  end
+
+  options.separator = ";"
+  opts.on("-s", "--separator SEPARATOR", "Use SEPARATOR as a column separator (standard: \"#{options.separator}\")") do |s|
+  	options.separator = s
+  end
+
+  options.headers = false
+  opts.on("-H", "--[no-]headers", "Skip first line in CSV input") do |d|
+  	options.headers = d
+  end
+
+  options.template = template
+  opts.on("-t", "--template TEMPLATE", "Use TEMPLATE as a JSON template for every place,\n                                     use \"\#\{_[n]\}\" to reference columns in CSV starting with n=0") do |t|
+  	options.template = open(t).read
+  end
+
+  options.commit = false
+  opts.on("-c", "--[no-]commit", "Commit changes to gazetteer instead of only printing them") do |c|
+  	options.commit = c
+  end
+
+  options.merge = false
+  opts.on("-m", "--[no-]merge", "Perform a merge with existing data if 'gazId' is present\n                                     IMPORTANT: otherwise places with existing gazetteer ids will be replaced!") do |m|
+    options.merge = m
+  end
+
+  options.replace = false
+  opts.on("-r", "--[no-]replace", "Replace existing data\n                                     when merging new data will have priority over existing data\n                                     when merging is switchted existing places with the same id will be replaced") do |r|
+    options.replace = r
+  end
+
+  options.verbose = false
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options.verbose = v
+  end
+
+  opts.on_tail("-h", "--help", "Show this message") do
+  puts opts
+    exit
+  end
+
+end.parse!
+
+# merge helper
+merger = lambda do |key, oldval, newval|
+  if oldval.is_a? Array
+    newval | oldval
+  elsif oldval.is_a? Hash
+    oldval.merge(newval)
+  else
+    newval
+  end
+end
+
+# main program
+
+gaz = RestClient::Resource.new(options.uri, :user => options.user, :password => options.password)
+total = 0
+inserted = 0
+skipped = 0
+merged = 0
+replaced = 0
+
+parsed_headers = false
+
+CSV.parse(ARGF.read, {:col_sep => options.separator}) do |row|
+  
+  if options.headers and !parsed_headers
+    parsed_headers = true
+    next
+  end
+
+  id_present = false
+
+  # create place object by applying template
+  _ = row
+  _.map! { |val| val.to_s } # convert nils to empty strings
+  eval_str = "\"#{options.template.gsub(/\"/){|m|"\\"+m}}\""
+  place = JSON.parse(eval(eval_str), :symbolize_names => true)
+
+  # postprocess to delete empty fields
+  place[:prefName].delete(:language) if place[:prefName][:language].to_s.empty?
+  place[:prefName].delete(:ancient) if !place[:prefName][:ancient]
+  if place[:alternativeNames] 
+    place[:alternativeNames].delete_if { |id| id[:title].to_s.empty? }
+  end
+  place.delete(:types) if place[:types].empty?
+  place.delete(:prefLocation) if place[:prefLocation][:coordinates] == [0, 0]
+  if place[:ids] 
+    place[:ids].delete_if { |id| id[:value].to_s.empty? }
+  end
+
+  id_present = true if !place[:gazId].to_s.empty?
+  total += 1
+
+  # check for existing place and apply merge if necessary
+  if id_present
+    begin
+      response = gaz["doc/#{place[:gazId]}"].get(:content_type => :json, :accept => :json)
+      existing_place = JSON.parse(response.body, :symbolize_names => true)
+      if !options.merge
+        if !options.replace
+          # skip place to prevent replacement of existing place
+          puts "skipping duplicate #{place[:gazId]}" if options.verbose
+          skipped += 1
+          next
+        else
+          # place will be replaced
+          puts "WARNING: place with gazetteer id #{place[:gazId]} will be replaced!"
+          replaced += 1
+        end
+      else
+        if !options.replace
+          # existing place has priority
+          place = place.merge(existing_place, &merger)
+        else
+          # new place has priority
+          place = existing_place.merge(place, &merger)
+        end
+        puts JSON.pretty_generate(place)
+        merged += 1
+      end
+    rescue RestClient::Exception => e
+      puts "WARNING: gazetteer id #{place[:gazId]} not present in gazetteer, generation of custom ids is not supported"
+      puts "HTTP response code: #{e.http_code}" if options.verbose
+      place.delete(:gazId)
+      inserted += 1
+    end
+  else
+    inserted += 1
+  end
+
+  # write data to gazetteer
+  if options.commit
+    begin
+      # perform POST to gazetteer API
+      if id_present
+        response = gaz["doc/#{place[:gazId]}"].put(place.to_json, :content_type => :json, :accept => :json)
+        puts "updated: " + JSON.parse(response.body)["@id"] if options.verbose
+      else
+        response = gaz['doc'].post(place.to_json, :content_type => :json, :accept => :json)
+        puts "created: " + JSON.parse(response.body)["@id"] if options.verbose
+      end
+    rescue RestClient::Exception => e
+      puts "ERROR: #{e.http_code}" if options.verbose
+      puts e.response.body if options.verbose
+      if e.http_code == 401
+        puts "ERROR: user name or password incorrect, aborting ..."
+        exit(1)
+      end
+    end
+  else
+    # dry run
+    puts JSON.pretty_generate(place) if options.verbose
+  end
+
+end
+
+puts "OK: read #{total} places"
+puts "inserted #{inserted}"
+puts "skipped #{skipped}"
+puts "merged #{merged}"
+puts "replaced #{replaced}"
+exit(0)
