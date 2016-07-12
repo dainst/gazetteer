2888dbc8fb1204cc442d4423d1cbeb456c5dc453
Thomas Kleinke <tkleinke@smail.uni-koeln.de>
2016-07-12 14:13:32 +0200

    better error handling for place names with invalid language (fixes #3179)

--- src/main/java/org/dainst/gazetteer/helpers/PlaceNameHelper.java
+++ src/main/java/org/dainst/gazetteer/helpers/PlaceNameHelper.java
@@ -7,11 +7,16 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.MissingResourceException;
 import java.util.Set;
 
 import org.dainst.gazetteer.domain.PlaceName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PlaceNameHelper {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PlaceNameHelper.class);
 		
 	private Locale locale;
 	
@@ -66,7 +71,7 @@ public class PlaceNameHelper {
 		public int compare(PlaceName placeName1, PlaceName placeName2) {
 
 			int langComp;
-						
+			
 			if ((placeName1.getLanguage() == null || placeName1.getLanguage().isEmpty())
 					&& (placeName2.getLanguage() != null && !placeName2.getLanguage().isEmpty()))
 				langComp = 1;
@@ -76,20 +81,34 @@ public class PlaceNameHelper {
 			else if ((placeName1.getLanguage() == null || placeName1.getLanguage().isEmpty())
 					&& (placeName2.getLanguage() == null || placeName2.getLanguage().isEmpty()))
 				langComp = 0;
-			else if (originalLocale.getISO3Language().equals(new Locale(placeName1.getLanguage()).getISO3Language())
-					&& !originalLocale.getISO3Language().equals(new Locale(placeName2.getLanguage()).getISO3Language()))
-				langComp = -1;
-			else if (originalLocale.getISO3Language().equals(new Locale(placeName2.getLanguage()).getISO3Language())
-					&& !originalLocale.getISO3Language().equals(new Locale(placeName1.getLanguage()).getISO3Language()))
-				langComp = 1;
 			else {
-				String localizedLanguage1 = localizedLanguages.get(placeName1.getLanguage());
-				String localizedLanguage2 = localizedLanguages.get(placeName2.getLanguage());
+				String originalLocaleIso3Language = null;
+				String placeName1Iso3Language = null;
+				String placeName2Iso3Language = null;
 				
-				if (localizedLanguage1 != null && localizedLanguage2 != null)
-					langComp = Collator.getInstance(locale).compare(localizedLanguage1, localizedLanguage2);
-				else
-					langComp = 0;
+				try {
+					originalLocaleIso3Language = originalLocale.getISO3Language();
+					placeName1Iso3Language = new Locale(placeName1.getLanguage()).getISO3Language();
+					placeName2Iso3Language = new Locale(placeName2.getLanguage()).getISO3Language();
+				} catch(MissingResourceException e) {
+					logger.warn("Failed to get ISO 3 language code", e);
+				}
+				
+				if (originalLocaleIso3Language.equals(placeName1Iso3Language)
+						&& !originalLocaleIso3Language.equals(placeName2Iso3Language))
+					langComp = -1;
+				else if (originalLocaleIso3Language.equals(placeName2Iso3Language)
+						&& !originalLocaleIso3Language.equals(placeName1Iso3Language))
+					langComp = 1;
+				else {
+					String localizedLanguage1 = localizedLanguages.get(placeName1.getLanguage());
+					String localizedLanguage2 = localizedLanguages.get(placeName2.getLanguage());
+					
+					if (localizedLanguage1 != null && localizedLanguage2 != null)
+						langComp = Collator.getInstance(locale).compare(localizedLanguage1, localizedLanguage2);
+					else
+						langComp = 0;
+				}
 			}
 			
 			if (langComp != 0)
