cd08c1a0cb0aff85ed203fe78c9ea8daaec4e2b4
tkleinke <tkleinke@smail.uni-koeln.de>
2016-10-25 23:52:10 +0200

    added check for duplicate points to fixPolygons.js

--- src/main/scripts/fixPolygons.js
+++ src/main/scripts/fixPolygons.js
@@ -2,24 +2,54 @@ connection = new Mongo();
 db = connection.getDB("gazetteer");
 
 db.place.find( { "prefLocation.shape" : { $exists : true } } ).forEach(
-	function(doc) {
-		if (doc.prefLocation.shape.coordinates && doc.prefLocation.shape.coordinates.length > 0) {
-			var coordinates = doc.prefLocation.shape.coordinates;
-			
-			// Polygone durchlaufen
-			for (var i = 0; i < coordinates.length; i++) {
-				
-				// Pfade durchlaufen
-				for (var j = 0; j < coordinates[i].length; j++) {
-					var path = coordinates[i][j];
-					var firstPoint = path[0];
-					var lastPoint = path[path.length - 1];
+        function(doc) {
 
-					if (firstPoint[0] != lastPoint[0] || firstPoint[1] != lastPoint[1])
-						path.push(firstPoint);
-				}
-			}
-		}
+                var invalidPolygon = false;
 
-		db.place.save(doc);
-	});
+                if (doc.prefLocation.shape.coordinates && doc.prefLocation.shape.coordinates.length > 0) {
+                        var coordinates = doc.prefLocation.shape.coordinates;
+
+                        // Iterate polygons
+                        for (var i = 0; i < coordinates.length; i++) {
+
+                                // Iterate paths
+                                for (var j = 0; j < coordinates[i].length; j++) {
+                                        var path = coordinates[i][j];
+                                        var firstPoint = path[0];
+                                        var lastPoint = path[path.length - 1];
+
+                                        if (firstPoint[0] != lastPoint[0] || firstPoint[1] != lastPoint[1]) {
+                                                path.push(firstPoint);
+                                                print("First point does not equal last point");
+                                                invalidPolygon = true;
+                                        }
+
+                                        // Iterate points
+                                        var points = {};
+                                        var pointsToRemove = [];
+
+                                        for (var k = 0; k < path.length; k++) {
+                                                var point = path[k];
+                                                if (points[point[0]] && points[point[0]] == point[1] && !(k == path.length - 1 && point[0] == firstPoint[0] && point[1] == firstPoint[1])) {
+                                                        print("Duplicate point " + point[0] + "," + point[1]);
+                                                        invalidPolygon = true;
+                                                        pointsToRemove.push(point);
+                                                } else {
+                                                        points[point[0]] = point[1];
+                                                }
+                                        }
+
+                                        for (var k = 0; k < pointsToRemove.length; k++) {
+                                                var index = path.indexOf(pointsToRemove[k]);
+                                                path.splice(index, 1);
+                                        }
+                                }
+                        }
+                }
+
+                db.place.save(doc);
+
+                if (invalidPolygon) {
+                        print("Fixed invalid polygon of place " + doc._id + "\n");
+                }
+        });
