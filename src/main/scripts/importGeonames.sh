15fbb53493e87fb30f79028f00ff555b9bafff73
MaximilianHeiden <mlh.heiden@gmail.com>
2018-06-14 12:43:53 +0200

    updated gazetteer URI
    http > https

--- src/main/scripts/importGeonames.sh
+++ src/main/scripts/importGeonames.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-curl -u admin:admin1 -XPOST "http://gazetteer.dainst.org/admin/importGeonames" > /var/log/gazetteer/importGeonames.log
+curl -u admin:admin1 -XPOST "https://gazetteer.dainst.org/admin/importGeonames" > /var/log/gazetteer/importGeonames.log
