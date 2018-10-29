f87e38cf759e44fd40d115d15f2b77e738ee43da
Simon Hohl <simon.hohl@dainst.org>
2018-10-29 15:59:18 +0100

    Fix mongo-connector -> elasticsearch connection

--- mongo-connector/docker-entrypoint.sh
+++ mongo-connector/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# wait until ES is up
+until $(curl -o /dev/null -s --head --fail $ELASTICSEARCH_CONTAINER_NAME:9200); do
+    echo "Waiting for ES to start..."
+    sleep 5
+done
+
+curl http://$ELASTICSEARCH_CONTAINER_NAME:9300/gazetteer/ -X PUT -H "Content-Type: application/json" -d "@/mappings/place_template.json"
+
 mongo-connector -m $MONGO_CONTAINER_NAME:27017 \
                 -t $ELASTICSEARCH_CONTAINER_NAME:9200 \
                 -d elastic2_doc_manager \
