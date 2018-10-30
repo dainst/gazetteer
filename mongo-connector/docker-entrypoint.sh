a816f4acb843fd259bfd4c8a71e2963c672158ad
Simon Hohl <simon.hohl@dainst.org>
2018-10-30 16:30:25 +0100

    Fix broken elasticsearch mapping PUT from mongo-connector

--- mongo-connector/docker-entrypoint.sh
+++ mongo-connector/docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# wait until ES is up
 until $(curl -o /dev/null -s --head --fail $ELASTICSEARCH_CONTAINER_NAME:9200); do
     echo "Waiting for ES to start..."
     sleep 5
 done
 
 echo "Pushing place template to elastic search..."
-curl http://$ELASTICSEARCH_CONTAINER_NAME:9300/gazetteer/ -X PUT -H "Content-Type: application/json" -d "@/mappings/place_template.json"
+curl http://$ELASTICSEARCH_CONTAINER_NAME:9200/gazetteer/ -X PUT -H "Content-Type: application/json" -d "@/mappings/place_template.json"
 
+echo ""
 echo "Starting connector..."
 mongo-connector -m $MONGO_CONTAINER_NAME:27017 \
                 -t $ELASTICSEARCH_CONTAINER_NAME:9200 \
