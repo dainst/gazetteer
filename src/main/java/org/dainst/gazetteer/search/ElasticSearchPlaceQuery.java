951cbe4195ca7a5506216957156c485a22ad8473
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-04-04 12:27:42 +0200

    fix prefix query

--- src/main/java/org/dainst/gazetteer/search/ElasticSearchPlaceQuery.java
+++ src/main/java/org/dainst/gazetteer/search/ElasticSearchPlaceQuery.java
@@ -93,8 +93,8 @@ public class ElasticSearchPlaceQuery {
 
 	public ElasticSearchPlaceQuery prefixSearch(String query) {
 		query = query.toLowerCase();
-		queryBuilder.should(QueryBuilders.termQuery("prefName.title.autocomplete", query))
-			.should(QueryBuilders.termQuery("names.title.autocomplete", query));
+		queryBuilder.must(QueryBuilders.boolQuery().should(QueryBuilders.termQuery("prefName.title.autocomplete", query))
+			.should(QueryBuilders.termQuery("names.title.autocomplete", query)));
 		return this;
 	}
 
