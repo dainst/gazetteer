da7121dc042f0394802ee9344de2d74293a7a6fc
Simon Hohl <simon.hohl@dainst.de>
2022-11-10 08:13:01 +0100

    Add google consent blocker

--- src/main/java/org/dainst/gazetteer/controller/AppController.java
+++ src/main/java/org/dainst/gazetteer/controller/AppController.java
@@ -7,7 +7,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.dainst.gazetteer.dao.GroupRoleRepository;
 import org.dainst.gazetteer.dao.RecordGroupRepository;
@@ -25,6 +27,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.servlet.support.RequestContext;
 
 @Controller
@@ -58,17 +61,38 @@ public class AppController {
 	
 	@Autowired
 	private GroupRoleRepository groupRoleDao;
-	
+
+
+	@RequestMapping(value="/consent")
+	public String consent(
+			@RequestParam(value="redirectTo") String redirectTo,
+			HttpServletResponse response
+	) {
+		logger.debug("Setting consent cookie and redirecting to " + redirectTo);
+		Cookie consent_cookie = new Cookie("gazetteer_google_consent", "1");
+		response.addCookie(consent_cookie);
+		return "redirect:" + redirectTo;
+	}
+
+
 	@Autowired
 	LanguagesHelper langHelper;
 	@RequestMapping(value="/app/")
-	public String app(ModelMap model, HttpServletRequest request, 
-			@RequestParam(value="_escaped_fragment_", required=false) String fragment) throws UnsupportedEncodingException {
-		
+	public String app(
+			ModelMap model, 
+			HttpServletRequest request, 
+			@RequestParam(value="_escaped_fragment_", required=false) String fragment,
+			@CookieValue(value="gazetteer_google_consent", required=false) String googleConsent
+			) throws UnsupportedEncodingException {
+
 		// render static html for crawlers
 		if (fragment != null && !fragment.isEmpty() && fragment.startsWith("/show")) {
 			String[] split = URLDecoder.decode(fragment, "UTF-8").split("/");
 			return "forward:/doc/" + split[2] + ".html";
+		}		
+		
+		if (googleConsent == null) {
+			return "app/consent";
 		}
 
 		Arrays.sort(idTypes, String.CASE_INSENSITIVE_ORDER);
@@ -86,9 +110,15 @@ public class AppController {
 		logger.debug("accept: {}", request.getHeader("Accept"));
 		return "app/index";
 	}
-	
+
+
 	@RequestMapping(value="/app/{view}.html")
-	public String app(@PathVariable String view, ModelMap model, HttpServletRequest request) {		
+	public String app(
+			@CookieValue(value="gazetteer_google_consent", required=false) String googleConsent, 
+			@PathVariable String view, 
+			ModelMap model, 
+			HttpServletRequest request
+	) {		
 		model.addAttribute("baseUri",baseUri);
 		Locale locale = new RequestContext(request).getLocale();
 		model.addAttribute("language", locale.getLanguage());
@@ -102,7 +132,12 @@ public class AppController {
 	}
 	
 	@RequestMapping(value="/app/partials/{view}.html")
-	public String appPartials(@PathVariable String view, ModelMap model, HttpServletRequest request) {
+	public String appPartials(
+			@CookieValue(value="gazetteer_google_consent", required=false) String googleConsent,
+			@PathVariable String view,
+			ModelMap model,
+			HttpServletRequest request
+	) {
 		User user = null;
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		if (principal != null && principal instanceof User) {
@@ -132,6 +167,5 @@ public class AppController {
 		model.addAttribute("placeTypeGroups", placeTypeGroups);
 		model.addAttribute("placeTypeGroupIds", placeTypeGroupIds);
 		return "app/partials/" + view;
-	}
-	
+	}	
 }
